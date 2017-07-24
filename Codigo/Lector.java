09a05d00854534a72ce840e99c7a4fe9a9871619
CoffeeBrewer <cas15151@uvg.edu.gt>
2017-07-23 22:35:48 -0600

    Agregando Primer Codigo

---
+++ Codigo/Lector.java
@@ -0,0 +1,21 @@
+/************************************************/
+/*Laboratorio 2				                    */
+/*UVG						                    */
+/*Diseno de Lenguajes                           */
+/*Tomas Galvez                                	*/
+/*Diego Castaneda 15151                       	*/
+/************************************************/
+
+/*****************************************************/
+/*Clase para lectura e identificacion y clasificacion*/
+/* de los datos importantes de la regex              */
+/*****************************************************/
+
+public class Lector(String regex){
+	
+	private String[] estados
+	private String[] simbolos
+
+	public Lector(){
+	}
+}
---
+++ Codigo/Main.java
@@ -0,0 +1,14 @@
+/************************************************/
+/*Laboratorio 2				                    */
+/*UVG						                    */
+/*Diseno de Lenguajes                           */
+/*Tomas Galvez                                	*/
+/*Diego Castaneda 15151                       	*/
+/************************************************/
+
+/*************************************************/
+/*Lectura de expresiones Regex y obtencion de sus*/
+/*estados, simbolos, nodo de inicio, nodos de    */
+/*aceptacion y transiciones posibles en la Regex */
+/************************************************/
+
