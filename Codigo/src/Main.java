f62692eefa4103d2d684d561d9ddc7cf03da35a0
CoffeeBrewer <cas15151@uvg.edu.gt>
2017-07-28 21:07:30 -0600

    Se crearon los algoritmos
    Para crear Arryalists que contengan a todos los Nodos del grafo, listas
    con todos los simbolos y listas con todas las transiciones dentro del
    grafo
    unicamente se necesita hacer su llamado en la clase Main e imprimir el
    resultado de estas listas, asi como el del nodo inicial y final del
    automata creado

--- Codigo/src/Main.java
+++ Codigo/src/Main.java
@@ -7,12 +7,23 @@ public class Main {
 
         System.out.println("Ingrese la expresion regular: ");
         String regex = sc.nextLine();
+        Controlador control = new Controlador(regex);
+
+        Automata t = control.LectorDeExpresiones();
+
+        /*Obtener el Nodo Inicial*/
+        /*Obtener el Nodo Final*/
+        /*Obtener la Numeracion de ID de nodos*/
+        /*Obtener las transiciones del grafo*/
+        /*Obtener los simbolos del grafo*/
 
         /*Se normalizo y postfixeo la expresion regular para que se tome en cuenta ? como (x|@)*/
         String RegexNormalizado = norm.PostFixYNormalizar(regex);
         String i = RegExConverter.infixToPostfix("abc");
         System.out.println(i);
         System.out.println(RegexNormalizado);
+
+
     }
 }
 
